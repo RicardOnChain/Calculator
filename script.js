8b2adb8130887b8928ef7d5d4d5f276078599413
Ricardo David <jose.ricardo.9720@gmail.com>
2025-04-13 20:59:20 +0100

    Finalized calculator MVP + Frontend adjustments

--- script.js
+++ script.js
@@ -1,35 +1,113 @@
 //Init Variablea
 let output=0
 let operator=""
-let var1=0
-let var2=0
-let displayList=[]
-let frstTimeNr=False
-let frstTimeOperator=False
-let scndTimeButton=False
+let var1
+let var2
+let displayList1=[]
+let displayList2=[]
+let isThisVar1=true
+let frstTimeOperator=true
+let endedEvaluation=false
 const displayRef=document.querySelector(".display")
+
+
+//Event Listeners
+
+document.querySelector(".pad").addEventListener("click", (e) => {
+
+    switch (e.target.id){
+        case "number":
+            if (endedEvaluation == true) {clear()}
+
+            if (isThisVar1 == true){
+                displayList1.push(e.target.innerText)
+                display()
+                break
+            
+            } else {
+                displayList2.push(e.target.innerText)
+                display(operator)
+                break
+            }
+
+        case "operator":
+            
+            endedEvaluation=false
+            
+            if ((e.target.innerText=="+" || e.target.innerText=="-") && Number.isNaN(parseFloat(displayList1.join("")))  ){
+                displayList1[0]=e.target.innerText
+                display()
+                break
+                }
+            
+            if (displayList2.length == 0){                                      // save var1 and reset display list 
+                operator=e.target.innerText                                            //when operator is introduced for the first time and numbers have already been introduced
+                display(operator)
+            
+                isThisVar1=false
+                frstTimeOperator=false
+                break
+
+            } else if (displayList2.length !=0){
+                var1= parseFloat(displayList1.join(""))                            //save var2 if an operator is pressed for a 2nd time and numbers have already been introduced
+                var2= parseFloat(displayList2.join(""))                            //save var2 if an operator is pressed for a 2nd time and numbers have already been introduced
+                operation (var1,var2,operator)
+
+                operator=e.target.innerText
+                display(operator)
+                break
+              
+            }
+        case "evaluate":
+            
+            var1= parseFloat(displayList1.join(""))                               
+            var2= parseFloat(displayList2.join(""))
+            if ( Number.isNaN(var1) || Number.isNaN(var2) ) {break}                 //run operations with var1 and var2 if they are defined
+            else {operation (var1,var2,operator)                                    
+            display ()
+            endedEvaluation = true
+            break
+            }
+
+        case "AC":
+            clear()
+            break
+
+        case "backspace":
+            backspace()
+            break
+        
+        case "perCent":
+            per100()
+            break
+}})
+
+
 
 
 //operations
 function add(a, b) {
     output = a + b
-    operator =""
-    displayList=[]
-    display (output)
+    displayList1=[]
+    displayList2=[]
+    displayList1.push(output)
+    display()
 }
 
 function subtract(a, b) {   
     output = a - b;
-    operator =""
-    displayList=[]
-    display (output)
+    displayList1=[]
+    displayList2=[]
+    displayList1.push(output)
+    display()
     
 }
 function multiply(a, b) {
     output = a * b;
-    operator =""
-    displayList=[]
-    display (output)
+    displayList1=[]
+    displayList2=[]
+    displayList1.push(output)
+    display()
     
 }
 
@@ -39,35 +117,68 @@ function divide(a, b) {
     } else {
         output = a / b;    
     }
-    operator =""
-    display (output)
+    displayList1=[]
+    displayList2=[]
+    displayList1.push(output)
+    display()
 
 }
 
 function clear(){
+    displayList1=[]
+    displayList2=[]
     output=0
-    operator =""
+    operator=""
+    isThisVar1=true
+    frstTimeOperator=true
+    endedEvaluation = false
+    display()
 }
 
 function backspace(){
+    if (isThisVar1 == true && frstTimeOperator == true){ 
+        displayList1.pop()
+    } else if (isThisVar1 == false && displayList2.length != 0){
+        displayList2.pop()
+    } else if (isThisVar1 == false && displayList2.length == 0){
+        operator=""
+        isThisVar1 = true
+    }
+    display(operator)
 
 }
 
-function per100(value){
-    if (result!=undefined) {output=value/100}
-    operator =""
-    displayList=[]
-    display (output)
+function per100(){
+    if (isThisVar1 == true && frstTimeOperator == true){ 
+        var1= parseFloat(displayList1.join(""))      
+        displayList1.push("%")
+        display(operator)   
+        displayList1=[(var1 / 100)]
+
+    } else if (isThisVar1 == false && displayList2.length != 0){
+        var1= parseFloat(displayList1.join(""))      
+        var2= parseFloat(displayList2.join(""))   
+        displayList2.push("%")
+        display(operator)   
+        displayList2=[var1*(var2 / 100)]
+
+    } else if (isThisVar1 == false && displayList2.length == 0){
+        var1= parseFloat(displayList1.join(""))
+        displayList1.push("%")
+        display(operator)   
+        displayList1=[(var1 / 100)]      
+
+    }
+
 
 }
 
-function display(value){
-    displayList.push(value)
-    displayRef.innerText = displayList.join("");
+function display(operator=""){
+    displayRef.innerText = (displayList1.join("")+ operator +displayList2.join(""));
 }
 
 function operation(a,b,operator){
-    scndTimeButton=False
+    
     switch (operator){
         case "+":
             add(a,b)
@@ -75,11 +186,16 @@ function operation(a,b,operator){
         case "-":
             subtract(a,b)
             break
-        case "*":
+        case "x":
             multiply(a,b)
             break
         case "/":
             divide(a,b)
             break
+    
+    scndTimeNr=False
+    var1=output
+    var2=undefined
+    
     }
 }
